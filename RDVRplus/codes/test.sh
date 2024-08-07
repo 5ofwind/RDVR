d145401ddfbc60df40a90050dc906ee26bbcfc48
5ofwind <96687612+5ofwind@users.noreply.github.com>
2024-08-07 18:11:55 +0800

    Add files via upload

---
+++ RDVRplus/codes/test.sh
@@ -0,0 +1,5 @@
+python3 test.py -opt options/test/test_RDVRplus_Vid4.yml
+
+python3 test.py -opt options/test/test_RDVRplus_UDM10.yml
+
+python3 test_vimeo.py -opt options/test/test_RDVRplus_Vimeo90K.yml
